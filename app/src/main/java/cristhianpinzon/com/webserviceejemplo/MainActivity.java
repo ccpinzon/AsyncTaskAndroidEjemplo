c153d45b1ca0d86f6804c048542f9a3f3d4390ce
ccpinzon <rdz.cristhian@gmail.com>
2017-03-01 20:23:31 -0500

    add metohd isOnline Conection to webservice

--- app/src/main/java/cristhianpinzon/com/webserviceejemplo/MainActivity.java
+++ app/src/main/java/cristhianpinzon/com/webserviceejemplo/MainActivity.java
@@ -1,17 +1,27 @@
 package cristhianpinzon.com.webserviceejemplo;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button boton;
     private TextView textView;
+    private ProgressBar progressBar;
+    List<MyTask> tasksList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +36,20 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textview);
         textView.setMovementMethod(new ScrollingMovementMethod());
 
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+
+        tasksList = new ArrayList<>();
+
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-//                for (int i = 0;i<=100;i++){
-//                    cargarDatos("Numero : " + i);
-//                }
-                MyTask task = new MyTask();
-                task.execute();
+                if (isOnline()){
+                    pedirDatos();
+                }else {
+                    Toast.makeText(getApplicationContext(),"SIN CONEXION ",Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
@@ -47,6 +62,34 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void pedirDatos(){
+
+        MyTask task = new MyTask();
+        // Forma Paralela
+        //task.execute();
+
+        //Forma seriada
+        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
+    }
+
+    //verificar Conectividad
+
+    public boolean isOnline() {
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if( networkInfo != null && networkInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
     //tarea en segundo plano que trae datos
     private class MyTask extends AsyncTask <String,String,String>{
 
@@ -54,10 +97,24 @@ public class MainActivity extends AppCompatActivity {
         protected void onPreExecute() {
             super.onPreExecute();
             cargarDatos("Iniciar de Cargar");
+            progressBar.setVisibility(View.VISIBLE);
+
+            // agregar task a  la lista
+            tasksList.add(this);
         }
 
         @Override
         protected String doInBackground(String... strings) {
+
+              for (int i = 0;i<=10;i++){
+                    //cargarDatos("Numero : " + i);
+                  publishProgress("Numero : " + i);
+                  try {
+                      Thread.sleep(100);
+                  } catch (InterruptedException e) {
+                      e.printStackTrace();
+                  }
+               }
             return "Terminamos";
         }
 
@@ -65,6 +122,18 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             cargarDatos(s);
+
+            tasksList.remove(this);
+
+            if (tasksList.isEmpty())
+                progressBar.setVisibility(View.INVISIBLE);
+
+
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            cargarDatos(values[0]);
         }
     }
 }
